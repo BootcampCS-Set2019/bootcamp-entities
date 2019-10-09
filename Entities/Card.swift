34b512e1ff13cab314f362ddf50e3682edfe257e
cs-eltonsantana <elton.faleta.santana@concrete.com.br>
2019-10-09 15:56:01 -0300

    Card conforming to NSManagedObject protocol

--- Entities/Card.swift
+++ Entities/Card.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2019 BootcampCS-Set2019. All rights reserved.
 //
 import Foundation
+import UIKit
+import CoreData
 
 public struct Cards: Codable {
     public let cards: [Card]
@@ -15,16 +17,58 @@ public struct Cards: Codable {
     }
 }
 
-public struct Card: Codable, Equatable {
+public class Card: NSManagedObject, Codable {
 
-    public let id: String
-    public let name: String
-    public let imageUrl: String?
+    @NSManaged public var id: String
+    @NSManaged public var name: String
+    @NSManaged public var imageUrl: String?
+    @NSManaged public var set: String
+    @NSManaged public var type: [String]
+//    public let image: UIImage?
 
-    public init( id: String, name: String, imageUrl: String) {
-        self.id = id
-        self.name = name
-        self.imageUrl = imageUrl
+    enum CodingKeys: String, CodingKey {
+        case id, name, imageURL, set, type
+    }
+//
+//    public init( id: String, name: String, imageUrl: String, set: String, type: [String]) {
+//        self.id = id
+//        self.name = name
+//        self.imageUrl = imageUrl
+//        self.set = set
+//        self.type = type
+////        self.image = image
+//    }
+
+    // MARK: - Decodable
+    override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+    public required convenience init(from decoder: Decoder) throws {
+        guard let codingUserInfoKeyManagedObjectContext = CodingUserInfoKey(rawValue: "context"),
+            let managedObjectContext = decoder.userInfo[codingUserInfoKeyManagedObjectContext] as? NSManagedObjectContext,
+            let entity = NSEntityDescription.entity(forEntityName: "Card", in: managedObjectContext) else {
+                fatalError("Failed to decode User")
+        }
+
+        self.init(entity: entity, insertInto: managedObjectContext)
+
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decodeIfPresent(String.self, forKey: .id)!
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)!
+        self.imageUrl = try container.decodeIfPresent(String.self, forKey: .imageURL)
+        self.set = try container.decodeIfPresent(String.self, forKey: .set)!
+        self.type = try container.decodeIfPresent(Array<String>.self, forKey: .type)!
+
+    }
+
+    // MARK: - Encodable
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.id, forKey: .id)
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.imageUrl, forKey: .imageURL)
+        try container.encode(self.set, forKey: .set)
+        try container.encode(self.type, forKey: .type)
     }
     
     public static func ==(lhs: Card, rhs: Card) -> Bool {
