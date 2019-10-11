39659a03454a322b87aaff0cb590c21a80b1a311
cs-eltonsantana <elton.faleta.santana@concrete.com.br>
2019-10-11 14:17:31 -0300

    Refactor Card model without NSManagedObject protocol

--- Entities/Card.swift
+++ Entities/Card.swift
@@ -17,58 +17,29 @@ public struct Cards: Codable {
     }
 }
 
-public class Card: NSManagedObject, Codable {
+public class Card: NSObject, Codable {
 
-    @NSManaged public var id: String
-    @NSManaged public var name: String
-    @NSManaged public var imageUrl: String?
-    @NSManaged public var set: String
-    @NSManaged public var type: [String]
-//    public let image: UIImage?
+    public var id: String
+    public var name: String
+    public var imageUrl: String?
+    public var set: String
+    public var types: [String]
+    public let imageData: Data?
 
-    enum CodingKeys: String, CodingKey {
-        case id, name, imageURL, set, type
-    }
-//
-//    public init( id: String, name: String, imageUrl: String, set: String, type: [String]) {
-//        self.id = id
-//        self.name = name
-//        self.imageUrl = imageUrl
-//        self.set = set
-//        self.type = type
-////        self.image = image
-//    }
-
-    // MARK: - Decodable
-    override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
-        super.init(entity: entity, insertInto: context)
-    }
-    public required convenience init(from decoder: Decoder) throws {
-        if let codingUserInfoKeyManagedObjectContext = CodingUserInfoKey(rawValue: "context"),
-            let managedObjectContext = decoder.userInfo[codingUserInfoKeyManagedObjectContext] as? NSManagedObjectContext,
-            let entity = NSEntityDescription.entity(forEntityName: "Card", in: managedObjectContext) {
-            self.init(entity: entity, insertInto: managedObjectContext)
-        } else {
-            let container = try decoder.container(keyedBy: CodingKeys.self)
-            self.id = try container.decodeIfPresent(String.self, forKey: .id)!
-            self.name = try container.decodeIfPresent(String.self, forKey: .name)!
-            self.imageUrl = try container.decodeIfPresent(String.self, forKey: .imageURL)
-            self.set = try container.decodeIfPresent(String.self, forKey: .set)!
-            self.type = try container.decodeIfPresent(Array<String>.self, forKey: .type)!
-        }
+    public init( id: String, name: String, imageUrl: String, set: String, types: [String], image: UIImage) {
+        self.id = id
+        self.name = name
+        self.imageUrl = imageUrl
+        self.set = set
+        self.types = types
+        self.imageData = image.jpegData(compressionQuality: 1)
     }
 
-    // MARK: - Encodable
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.id, forKey: .id)
-        try container.encode(self.name, forKey: .name)
-        try container.encode(self.imageUrl, forKey: .imageURL)
-        try container.encode(self.set, forKey: .set)
-        try container.encode(self.type, forKey: .type)
-    }
+
+
     
     public static func ==(lhs: Card, rhs: Card) -> Bool {
         return lhs.id == rhs.id
     }
 }
+
